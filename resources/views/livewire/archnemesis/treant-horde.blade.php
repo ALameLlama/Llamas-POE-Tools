e602b8c9532ab18897995549e5c012739691aa1c
Nicholas C <NicholasACiechanowskI@gmail.com>
2022-02-23 23:46:12 +1100

    feat: collapse

--- resources/views/livewire/archnemesis/treant-horde.blade.php
+++ resources/views/livewire/archnemesis/treant-horde.blade.php
@@ -1,8 +1,18 @@
-<div>
+<div x-data={show:true}>
     <div class="divide-y divide-gray-200 w-36 {{ $owned ? 'bg-green-400 dark:bg-green-600' : ($childOwned ? 'bg-yellow-400 dark:bg-yellow-600' : 'bg-red-400 dark:bg-red-600')}}">
         <div class="relative flex items-center justify-between mr-2 ml-2 h-8">
+
             <div class="text">
-                <label for="sentinel" class="font-medium text-gray-700">Sentinel</label>
+                <div class="flex items-center">
+                    <div @click="show=!show" class="transition" :class="show ? 'rotate-90' : ''">
+                        <svg class="w-4 h-4" data-darkreader-inline-stroke="" fill="none" stroke="currentColor" style="--darkreader-inline-stroke:currentColor;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7">
+
+                            </path>
+                        </svg>
+                    </div>
+                    <label for="sentinel" class="font-medium text-gray-700">Sentinel</label>
+                </div>
             </div>
             <div class="">
                 <input wire:click="toggle"
@@ -16,7 +26,9 @@
             </div>
         </div>
     </div>
-    @livewire('archnemesis.toxic', ['parent' => $parent])
-    @livewire('archnemesis.steel-infused', ['parent' => $parent])
-    @livewire('archnemesis.sentinel', ['parent' => $parent])
+    <div x-show="show" x-collapse>
+        @livewire('archnemesis.toxic', ['parent' => $parent])
+        @livewire('archnemesis.steel-infused', ['parent' => $parent])
+        @livewire('archnemesis.sentinel', ['parent' => $parent])
+    </div>
 </div>
